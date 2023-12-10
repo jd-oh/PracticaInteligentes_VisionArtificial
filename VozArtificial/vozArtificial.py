b75f478c3d517e24f40adee6cf1fc929df140a21
Juan David Osorio <72204602+jd-oh@users.noreply.github.com>
2023-12-10 17:42:54 -0500

    Voz artificial

--- VozArtificial/vozArtificial.py
+++ VozArtificial/vozArtificial.py
@@ -1,20 +1,32 @@
 import pyttsx3
+from flask import Flask, jsonify, request
+import threading
 
 # Crear un objeto de motor de texto a voz
 engine = pyttsx3.init()
+app = Flask(__name__)
 
-# Texto que deseas convertir en voz
-col=1
-fil=2
-mensaje = f"El siguiente movimiento es en la columna numero {col} y en la fila numero {fil}"
 
-# Configura la voz y el idioma (puedes ajustar la voz según tus preferencias)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)  # Elige la primera voz disponible
 
-# Reproduce el mensaje
-engine.say(mensaje)
-engine.runAndWait()
+@app.route('/receive-move', methods=['POST'])
+def receive_move():
+    # Recibe los datos del movimiento en formato JSON
+    data = request.get_json()
+    col = data['mensaje']
 
-# Cierra el motor de texto a voz
-engine.stop()
+    # Convierte el movimiento en voz
+    mensaje = f"Mi siguiente movimiento es en la columna numero {col}"
+    engine.say(mensaje)
+
+    # Verifica si el motor ya está en ejecución
+    if engine._inLoop:
+        engine.endLoop()
+
+    # Ejecuta engine.runAndWait() en un hilo separado
+    threading.Thread(target=engine.runAndWait).start()
+
+    return jsonify({'status': 'success'}), 200
+
+
+
+app.run(host='localhost', port=5001)
