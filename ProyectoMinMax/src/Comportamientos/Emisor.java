097d97b992ffb171c5137ebe8f8e13c37b9a318a
jeffrey <jloaiza@consensussa.com>
2023-12-10 17:15:00 -0500

    Conexion a api voz

--- ProyectoMinMax/src/Comportamientos/Emisor.java
+++ ProyectoMinMax/src/Comportamientos/Emisor.java
@@ -6,15 +6,15 @@ package Comportamientos;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import controllers.MinMaxFunctions;
-import jade.core.Agent;
-import jade.core.behaviours.SimpleBehaviour;
-import jade.lang.acl.ACLMessage;
+
 import java.util.List;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  *
@@ -25,7 +25,7 @@ public class Emisor {
     String respuesta;
 
     public Emisor() {
-        
+
     }
 
     public String getRespuesta() {
@@ -36,17 +36,15 @@ public class Emisor {
         String apiUrl = "http://127.0.0.1:5000/get-game-state";
 
         try {
-            // Crear una URL y abrir la conexión
+
             URL url = new URL(apiUrl);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-            // Configurar el tipo de solicitud y método
             con.setRequestMethod("GET");
 
-            // Obtener la respuesta de la API
             int responseCode = con.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Leer la respuesta de la API
+
                 BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 String inputLine;
                 StringBuilder response = new StringBuilder();
@@ -56,46 +54,81 @@ public class Emisor {
                 }
                 in.close();
 
-                // Guardar la respuesta en la variable "respuesta"
-                respuesta = response.toString();
-                
-                ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            List<List<String>> tablero = objectMapper.readValue(respuesta, List.class);
-            MinMaxFunctions function = new MinMaxFunctions();
+                if (respuesta != response.toString()) {
 
-            // Imprimir el tablero
-            System.out.println("Tablero Actual:");
-            imprimirTablero(tablero);
+                    respuesta = response.toString();
 
-            // Calcular la próxima jugada usando el algoritmo minimax
-            int[] movimiento = function.encontrarMejorMovimiento(tablero, "C");
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    try {
+                        List<List<String>> tablero = objectMapper.readValue(respuesta, List.class);
+                        MinMaxFunctions function = new MinMaxFunctions();
 
-            // Imprimir la próxima jugada
-            System.out.println("\nPróxima Jugada:");
-            System.out.println("Fila: " + movimiento[0] + ", Columna: " + movimiento[1]);
+                        System.out.println("Tablero Actual:");
+                        imprimirTablero(tablero);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-            
-        } else {
+                        // Calcular la próxima jugada usando el algoritmo minimax
+                        int[] movimiento = function.encontrarMejorMovimiento(tablero, "C");
+                        
+                        String jugada = "Fila: " + movimiento[0] + ", Columna: " + movimiento[1];
+                        this.anunciarJugada(jugada);
+                        
+                        System.out.println("\nPróxima Jugada:");
+                        System.out.println(jugada);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
                 System.out.println("La solicitud GET no fue exitosa. Código de respuesta: " + responseCode);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public void anunciarJugada(){
-        
+
+    public void anunciarJugada(String jugada) {
+        String apiUrl = "http://127.0.0.1:5001/receive-move";
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json");
+
+            // Habilitar el envío de datos
+            con.setDoOutput(true);
+
+            // Datos que deseas enviar en el cuerpo de la solicitud (cambia según tus necesidades)
+            String datos = "{\"mensaje\":\"" + jugada + "\"}";
+
+            // Obtener el flujo de salida y escribir los datos en el cuerpo de la solicitud
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = datos.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // Obtener la respuesta de la API
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-     private static void imprimirTablero(List<List<String>> tablero) {
+
+    private static void imprimirTablero(List<List<String>> tablero) {
         for (List<String> fila : tablero) {
             System.out.println(fila);
         }
     }
-
 
 }
